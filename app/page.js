d8b50981ec942b1be479fd4b2d41b8d95e36f527
muhammad-salman-saleem <msalman1221998@gmail.com>
2024-07-04 18:18:15 +0500

    next testing app

---
+++ app/page.js
@@ -0,0 +1,105 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="Main w-[1594px] h-[1164px] relative bg-white">
+  <div className="Group7 w-[1594px] h-[1024px] left-0 top-0 absolute">
+    <div className="Rectangle8 w-[1594px] h-[937px] left-0 top-[87px] absolute bg-neutral-100" />
+    <img className="Image2 w-[1594px] h-[1024px] left-0 top-0 absolute" src="https://via.placeholder.com/1594x1024" />
+    <div className="Image3 w-[1594px] h-[1024px] left-0 top-0 absolute bg-gradient-to-b from-neutral-200 via-white to-zinc-900" />
+    <div className="Group3 origin-top-left rotate-[-15.48deg] w-[463.43px] h-[132.23px] left-[153.87px] top-[337.67px] absolute">
+      <div className="Ellipse1 w-[143.82px] h-[132.23px] left-0 top-0 absolute origin-top-left rotate-[-15.48deg] bg-blue-600 rounded-full" />
+      <div className="Ellipse3 w-[143.82px] h-[132.23px] left-[308.01px] top-[-85.29px] absolute origin-top-left rotate-[-15.48deg] bg-rose-600 rounded-full" />
+      <div className="Ellipse2 w-[143.82px] h-[132.23px] left-[130.91px] top-[-36.25px] absolute origin-top-left rotate-[-15.48deg] bg-white rounded-full" />
+    </div>
+    <div className="Group4 origin-top-left rotate-[-42.09deg] w-[421.09px] h-[130.68px] left-[1045.02px] top-[653.30px] absolute">
+      <div className="Ellipse1 w-[130.68px] h-[130.68px] left-[-0px] top-0 absolute origin-top-left rotate-[-42.09deg] bg-blue-600 rounded-full" />
+      <div className="Ellipse3 w-[130.68px] h-[130.68px] left-[215.49px] top-[-194.67px] absolute origin-top-left rotate-[-42.09deg] bg-rose-600 rounded-full" />
+      <div className="Ellipse2 w-[130.68px] h-[130.68px] left-[91.58px] top-[-82.74px] absolute origin-top-left rotate-[-42.09deg] bg-white rounded-full" />
+    </div>
+    <div className="Group5 origin-top-left rotate-[134.60deg] w-[410.40px] h-[128.86px] left-[554.91px] top-[430.26px] absolute">
+      <div className="Ellipse12 w-[127.36px] h-[128.86px] left-0 top-0 absolute origin-top-left rotate-[134.60deg] bg-blue-600 rounded-full" />
+      <div className="Ellipse32 w-[127.36px] h-[128.86px] left-[-198.74px] top-[201.52px] absolute origin-top-left rotate-[134.60deg] bg-rose-600 rounded-full" />
+      <div className="Ellipse22 w-[127.36px] h-[128.86px] left-[-84.46px] top-[85.65px] absolute origin-top-left rotate-[134.60deg] bg-white rounded-full" />
+    </div>
+  </div>
+  <div className="Group89 w-[1399.18px] h-[633px] left-[92.98px] top-[199px] absolute">
+    <div className="Frame13 w-[1399.18px] h-[633px] p-2.5 left-0 top-0 absolute flex-col justify-start items-start gap-2.5 inline-flex">
+      <div className="Rectangle7 self-stretch h-[613px] bg-white bg-opacity-90 rounded-xl shadow border-2 border-white" />
+    </div>
+    <div className="Frame12 w-[1294.02px] h-[633px] left-[104.05px] top-0 absolute justify-center items-center inline-flex">
+      <div className="Frame10 grow shrink basis-0 h-[333px] flex-col justify-start items-start inline-flex">
+        <div className="Frame5 self-stretch h-52 flex-col justify-start items-start flex">
+          <div className="Frame4 self-stretch p-2.5 justify-start items-center gap-2.5 inline-flex">
+            <div className="AreYouReadyTo grow shrink basis-0 text-black text-3xl font-normal font-['HelveticaNeue'] leading-[45px]">Are you ready to</div>
+          </div>
+          <div className="Frame3 self-stretch h-[174px] flex-col justify-start items-start flex">
+            <div className="Frame2 self-stretch p-2.5 justify-start items-center gap-2.5 inline-flex">
+              <div className="EnhanceYour grow shrink basis-0 text-rose-600 text-[64px] font-normal font-['HelveticaNeue'] leading-[96px]">Enhance Your</div>
+            </div>
+            <div className="Frame1 self-stretch py-2.5 justify-start items-center gap-2.5 inline-flex">
+              <div className="LearningExperience text-blue-900 text-[64px] font-normal font-['HelveticaNeue'] leading-[96px]">Learning Experience?</div>
+            </div>
+          </div>
+        </div>
+        <div className="Frame6 self-stretch p-2.5 justify-center items-center gap-2.5 inline-flex">
+          <div className="EnterACodeSubmittedByYourOrganization grow shrink basis-0 text-black text-lg font-normal font-['HelveticaNeue'] leading-[27px]">Enter a code submitted by your organization</div>
+        </div>
+        <div className="Frame9 self-stretch justify-start items-start gap-1 inline-flex">
+          <div className="Frame7 grow shrink basis-0 p-2.5 flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="Textfield w-[301.50px] h-[43px] justify-center items-center inline-flex">
+              <div className="Textprimitive w-[302px] h-[43px] pl-3 pr-3.5 py-3 bg-white rounded border border-zinc-300 justify-center items-center inline-flex">
+                <div className="Input w-[276px] h-[19px] justify-center items-center inline-flex">
+                  <div className="JaneDoe w-[276px] text-zinc-500 text-base font-['HelveticaNeue'] leading-normal">Jane Doe</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="Frame8 grow shrink basis-0 p-2.5 flex-col justify-center items-start gap-2.5 inline-flex">
+            <div className="Buttonsimpledefault self-stretch justify-start items-center inline-flex">
+              <div className="Buttonprimitivedefault px-[17.20px] py-[8.60px] bg-rose-600 rounded justify-start items-center flex">
+                <div className="Button text-center text-white text-lg font-normal font-['HelveticaNeue'] leading-relaxed">Send</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Frame11 grow shrink basis-0 pl-[111px] pr-[11px] py-2.5 flex-col justify-center items-end gap-2.5 inline-flex">
+        <img className="Rectangle9 self-stretch h-[613px] rounded-xl border-2 backdrop-blur-[2px]" src="https://via.placeholder.com/525x613" />
+      </div>
+    </div>
+  </div>
+  <div className="Header w-[1594px] h-[88px] left-0 top-[2px] absolute">
+    <div className="Rectangle10 w-[1594px] h-[88px] left-0 top-0 absolute bg-white bg-opacity-80 backdrop-blur-sm" />
+    <div className="Line1 w-[1594px] h-[0px] left-0 top-[88px] absolute border border-neutral-200"></div>
+    <div className="Group86 w-[110px] h-[39.75px] left-[102px] top-[21.56px] absolute">
+      <div className="Practle left-0 top-0 absolute text-black text-2xl font-extrabold font-['Manrope']">Practle</div>
+      <div className="YourLearningJourney left-0 top-[24.75px] absolute text-black text-[11.15px] font-medium font-['Manrope']">Your learning journey</div>
+    </div>
+    <div className="MyLearningJourney left-[1121px] top-[34px] absolute text-black text-base font-normal font-['HelveticaNeue'] leading-normal">My Learning Journey</div>
+    <div className="Buttonsimpledefault w-[191px] h-[45px] left-[1305px] top-[22px] absolute">
+      <div className="Buttonprimitivedefault w-[191px] px-[17.20px] py-[8.60px] left-[0.34px] top-[0.40px] absolute bg-blue-900 rounded justify-start items-center inline-flex">
+        <div className="Button2 text-white text-lg font-normal font-['HelveticaNeue'] leading-relaxed">        My Profile </div>
+      </div>
+      <div className="Chevrondown w-5 h-5 pl-[5px] pr-[4.99px] pt-[6.99px] pb-[7px] left-[155px] top-[13px] absolute justify-center items-center inline-flex" />
+      <div className="User w-5 h-5 px-[3px] pt-[3px] pb-0.5 left-[14px] top-[11px] absolute justify-center items-center inline-flex" />
+    </div>
+  </div>
+  <div className="Footer w-[1594px] h-[140px] left-0 top-[1024px] absolute bg-red-600">
+    <div className="Rectangle11 w-[1594px] h-24 left-0 top-[44px] absolute bg-blue-900 backdrop-blur-sm" />
+    <div className="Rectangle12 w-[1594px] h-11 left-0 top-0 absolute bg-rose-600 backdrop-blur-sm" />
+    <div className="WhatYouShouldKnowAboutUs left-[678px] top-[12px] absolute text-white text-base font-normal font-['HelveticaNeue'] leading-normal">What you should know about us</div>
+    <div className="Group6 w-[498px] h-[22px] left-[546px] top-[71px] absolute">
+      <div className="TermsAndConditions left-0 top-[1px] absolute text-white text-sm font-normal font-['HelveticaNeue'] leading-[21px]">Terms and Conditions</div>
+      <div className="CookiePolicy left-[182px] top-[1px] absolute text-white text-sm font-normal font-['HelveticaNeue'] leading-[21px]">Cookie Policy</div>
+      <div className="Line2 w-[21px] h-[0px] left-[167px] top-[21px] absolute origin-top-left -rotate-90 border border-white"></div>
+      <div className="Line3 w-[21px] h-[0px] left-[289px] top-[21px] absolute origin-top-left -rotate-90 border border-white"></div>
+      <div className="Line4 w-[21px] h-[0px] left-[394px] top-[21px] absolute origin-top-left -rotate-90 border border-white"></div>
+      <div className="Disclaimer left-[304px] top-[1px] absolute text-white text-sm font-normal font-['HelveticaNeue'] leading-[21px]">Disclaimer</div>
+      <div className="PrivacyPolicy left-[406px] top-[1px] absolute text-white text-sm font-normal font-['HelveticaNeue'] leading-[21px]">Privacy Policy</div>
+    </div>
+    <div className="Copyright2024AllRightsReserved left-[690.73px] top-[113px] absolute text-white text-xs font-normal font-['HelveticaNeue'] leading-[18px]">Copyright ©2024. All rights reserved.</div>
+  </div>
+</div>
+  );
+}
